313d39ea31477ecc440129db1edcb9cb80313641
nera1 <nera4936@gmail.com>
2025-03-05 17:44:07 +0900

    feat: Implement Redux for state management #4

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { useAppDispatch } from "@/redux/hooks";
+import { open } from "@/redux/slices/sheet-slice";
 
 import styles from "@/styles/header.module.scss";
 
 export default function Header() {
+  const dispatch = useAppDispatch();
+
+  function onClickAvatar() {
+    dispatch(open());
+  }
+
   return (
     <header className={`${styles["header"]} flex justify-center`}>
       <div className={`${styles["container"]} flex`}>
         <div className="flex-1 border justify-start"></div>
         <div className="flex-1 flex items-center border justify-end pr-4">
-          <Avatar className="cursor-pointer">
+          <Avatar className="cursor-pointer" onClick={onClickAvatar}>
             <AvatarImage
               src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/woman-wearing-fencing-mask-portrait-stasker.jpg"
-              alt="@shadcn"
+              alt="avatar"
             />
             <AvatarFallback>PR</AvatarFallback>
           </Avatar>
